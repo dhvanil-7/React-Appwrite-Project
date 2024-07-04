645bc9b25322dc7545fdd116877b5d7d0f3c3cc1
dhvanil-7 <dkprajapati46@gmail.com>
2024-07-04 19:00:36 -0400

    chore: add components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,39 @@
-import conf from './conf/conf.js'
+import { useState } from 'react'
+import {useDispatch} from 'react-redux'
+import authService from './appwrite/auth'
+import { useEffect } from 'react'
+import { login, logout } from './store/authSlice'
+import { Header, Footer } from './components'
 
 function App() {
-  console.log(conf.VITE_APPWRITE_ENDPOINT_URL)
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
 
-  return (
-    <>
-      <h1>Blog with appwrite</h1>
-    </>
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if(userData){
+        dispatch(login({userData}))
+      }
+      else{
+        dispatch(logout())
+      }
+    })
+    .finally(() => setLoading(false))
+  }, [])
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header/>
+        <main>
+          TODO: {/*outlet*/}
+        </main>
+        <Footer/>
+      </div>
+    </div>
+  ) : (
+    <div>hello</div>
   )
 }
 
--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -4,7 +4,6 @@ import { Client, Account, ID } from "appwrite";
 
 export class AuthService {
     client = new Client()
-    Account
 
     constructor(){
         this.client.setEndpoint(conf.appwriteUrl)
