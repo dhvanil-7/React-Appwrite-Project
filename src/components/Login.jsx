dbbf4ff9722d99c69e62907c536382ef911fcc75
dhvanil-7 <dkprajapati46@gmail.com>
2024-07-05 22:49:09 -0400

    fix: bugs

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,6 @@
 import React, {useState} from 'react'
 import { Link, useNavigate } from 'react-router-dom'
-import { login } from '../store/authSlice'
+import { login as authLogin } from '../store/authSlice'
 import {Button, Input, Logo} from './index'
 import { useDispatch } from 'react-redux'
 import {useForm} from "react-hook-form"
@@ -19,7 +19,7 @@ function Login() {
             if(session){
                 const userData = await authService.getCurrentUser()
                 if(userData){
-                    dispatch(login(userData))
+                    dispatch(authLogin(userData))
                 }
                 navigate("/")
             }
