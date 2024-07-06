b4809eec03326f96af723b473cc0e1927e7f8562
dhvanil-7 <dkprajapati46@gmail.com>
2024-07-06 00:39:01 -0400

    fix: bugs

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,7 +8,6 @@ function Home() {
     useEffect(() => {
         appwriteService.getPosts().then((posts) => {
             if(posts){
-                console.log(posts)
                 setPosts(posts.documents)
             }
         })
@@ -29,21 +28,23 @@ function Home() {
             </div>
         )
     }
-  return (
-    <div className='w-full py-8'>
-        <Container>
-            <div className='flex flex-wrap'>
-                {posts.map((post) => (
-                    <div key={post.$id} className='p-2 w-1/4'>
-                        <PostCard {...post} />
-                    </div>
-                ))}
+    else {
+        return (
+            <div className='w-full py-8'>
+                <Container>
+                    <div className='flex flex-wrap'>
+                        {posts.map((post) => (
+                            <div key={post.$id} className='p-2 w-1/4'>
+                                <PostCard {...post} />
+                            </div>
+                        ))}
 
+                    </div>
+                </Container>
+            
             </div>
-        </Container>
-      
-    </div>
-  )
+        )
+    }
 }
 
 export default Home
